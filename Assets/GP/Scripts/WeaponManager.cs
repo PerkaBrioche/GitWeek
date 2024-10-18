4552a2e03f4dbd522c86d0dd080c22a04996964d
imriiama <146062070+imriiama@users.noreply.github.com>
2024-10-18 10:52:55 +0200

    Reapply "Reapply "Merge branch 'RaphaelGD' into dev-maria""
    This reverts commit 2c6745c5ad51737af303e130d0e41b6680937e8d.

---
+++ Assets/GP/Scripts/WeaponManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager Instance;
+    [NonSerialized] public WeaponData PlayerWeapon;
+    public WeaponController WeaponController;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        NewWeapon(0);
+    }
+    public List<WeaponData> LIST_AllWeapons;
+    
+    WeaponData GetActualWeapon()
+    {
+        return PlayerWeapon;
+    }
+    public void NewWeapon(int Index)
+    {
+        PlayerWeapon = LIST_AllWeapons[Index].Clone();
+        WeaponController.UpdateWeapon(PlayerWeapon);
+    }
+}
