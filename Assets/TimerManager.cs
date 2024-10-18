fd601b399a67ee0bbf5922d0cffbdd59da9ff328
imriiama <146062070+imriiama@users.noreply.github.com>
2024-10-18 09:51:33 +0200

    Reapply "Merge branch 'RaphaelGD' into dev-maria"
    This reverts commit 3d309b3918db4fa58c0cc6be89fdf13572affee4.

---
+++ Assets/TimerManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    public Slider SLID_Timer;
+    private bool BOOL_TimerRunning;
+
+    public float FLO_MaxTimer;
+    public float FLO_Multiplier;
+
+    public static TimerManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        FLO_Multiplier = 1;
+        LaunchTimer();
+    }
+
+    private void Update()
+    {
+        if (BOOL_TimerRunning)
+        {
+            ReduceTimer();
+        }
+
+        if (SLID_Timer.value <= 0)
+        {
+            StopTimer();
+            GameManager.Instance.GameOver();
+        }
+    }
+
+    private void ReduceTimer()
+    {
+        SLID_Timer.value -= Time.deltaTime * FLO_Multiplier;
+    }
+
+    public void LaunchTimer()
+    {
+        SLID_Timer.maxValue = FLO_MaxTimer;
+        SLID_Timer.value = FLO_MaxTimer;
+        BOOL_TimerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        BOOL_TimerRunning = false;
+    }
+
+    public void AddToTimer(float TimeToAdd)
+    {
+        SLID_Timer.value += TimeToAdd;
+    }
+    
+    public void SoustractTimer(float TimeToSub)
+    {
+        SLID_Timer.value -= TimeToSub;
+    }}
